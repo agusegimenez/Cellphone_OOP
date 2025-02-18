00cde146361c662bda110b356457cc5d166383a8
lema-23 <gonzalo1lema@gmail.com>
2025-02-18 18:12:06 -0300

    Modified class Reunion

--- Cellphone_OOP/src/Models/Reunion.java
+++ Cellphone_OOP/src/Models/Reunion.java
@@ -8,6 +8,18 @@ public class Reunion extends Evento{
     //Constructor de la clase Reunion
     public Reunion(int idEvento, Date fecha, int duracion, int cantidadParticipantes, String motivo) {
         super(idEvento, fecha, duracion, cantidadParticipantes, motivo);
+        gestionarSala(); // Asigna el tipo de sala al crear la reunión
+    }
+
+    // Metodo para asignar sala segun cantidad de participantes
+    public void gestionarSala() {
+        if (getCantidadParticipantes() <=5) {
+            sala = TipoSala.CHICA;
+        } else if (getCantidadParticipantes() <=15) {
+            sala = TipoSala.MEDIANA;
+        } else {
+            sala = TipoSala.GRANDE;
+        }
     }
 
     //getters y setters de los atributos de la clase Reunion
